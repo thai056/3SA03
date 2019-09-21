dd61fc15455b863ae72b17b1142a38107d658f10
thai056 <jearananthai@gmail.com>
2019-09-22 02:45:11 +0700

    Modify 7

--- Test.js
+++ Test.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import './Test.css';
-
 class Test extends React.Component {
     render() {
-        let count_end = this.props.check_count > 5 ? 'area2' : '';      //check count //
-        let count_ends = this.props.check_count > 5 ? "Game Over !!" : "Time : " + this.props.check_count;  //check count end //
+        let count_end = this.props.check_count > 5 ? 'area2' : '';      //check count 
+        let count_ends = this.props.check_count > 5 ? "Game Over !!" : "Time : " + this.props.check_count;  //check count when ended
         let test = `area ${count_end}`
         return (
             <div>
