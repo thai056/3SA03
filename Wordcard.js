771c5f314a277d8179eac3faff767f504e889d1f
thai056 <jearananthai@gmail.com>
2019-09-22 02:45:17 +0700

    Modify 8

--- Wordcard.js
+++ Wordcard.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 
-export default class Wordcard extends Component {
-    render() {
+export default class Wordcard extends Component{
+    render(){
         return (
             <div>{this.props.value}</div>
         )
